5cb26e1e31c6139082a007c3ac6f400774c81979
fanta-git <fantaji4957t@gmail.com>
2022-05-22 02:34:37 +0900

    add: タイトルと投稿者名の編集機能の追加#2

--- src/PlaylistTable.tsx
+++ src/PlaylistTable.tsx
@@ -7,14 +7,31 @@ type Props = {
     tableData: SongDataForTable[]
 }
 
+const toCamelCase = (str: string) => {
+    return str.split('-').map((word, index) => index
+        ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        : word.toLowerCase()
+    ).join('');
+};
+
 const PlaylistTable: React.FC<Props> = ({ tableData }) => {
     const items: React.ReactElement[] = [];
+    const updateContent: React.MouseEventHandler<HTMLDivElement> = async (event) => {
+        const elm = event.currentTarget;
+        const fullContent = elm.parentElement!.dataset[toCamelCase(elm.className)];
+        const newContent = await window.api.electronPrompt({
+            title: 'title',
+            label: 'label',
+            value: fullContent
+        });
+        if (newContent !== null) elm.textContent = newContent;
+    };
     for (const videoData of tableData) {
         items.push(
-            <div className="song" key={items.length}>
+            <div className="song" key={items.length} data-title={videoData.title} data-user-name={videoData.userName}>
                 <img className="thumbnail" src={videoData.thumbnail} />
-                <div className="title">{videoData.title}</div>
-                <div className="creator">{videoData.userName}</div>
+                <div className="title" onClick={updateContent}>{videoData.title}</div>
+                <div className="user-name" onClick={updateContent}>{videoData.userName}</div>
             </div>
         );
     }
--- src/apis/electronPrompt.ts
+++ src/apis/electronPrompt.ts
@@ -2,7 +2,7 @@ import { ipcRenderer } from "electron";
 import type electronPrpmptT from 'electron-prompt';
 
 const electronPrompt = async (options?: electronPrpmptT.Options) => {
-    const result = await ipcRenderer.invoke('electronPrompt', options);
+    const result = await ipcRenderer.invoke('electronPrompt', options) as string | null;
     return result;
 };
 
