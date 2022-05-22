48be981bf84bc9f566a5dbb27adb8381e2afc236
fanta-git <fantaji4957t@gmail.com>
2022-05-22 13:45:32 +0900

    add: ハンドラー関数とかをViewMenuに移動

--- src/ViewerMenu.tsx
+++ src/ViewerMenu.tsx
@@ -1,34 +1,57 @@
 import React from "react";
+import { SongDataForTable } from "./types";
+import domtoimage from 'dom-to-image';
 
 type Props = {
-    urlInputRef: React.MutableRefObject<HTMLInputElement>,
-    csvInputRef: React.MutableRefObject<HTMLInputElement>,
-    loadList: React.MouseEventHandler<HTMLButtonElement>,
-    outputJpeg: React.MouseEventHandler<HTMLButtonElement>
+    setPlaylistData: React.Dispatch<React.SetStateAction<SongDataForTable[]>>
 };
 
 const ViewerMenu: React.FC<Props> = props => {
+    const urlInputRef = React.useRef<HTMLInputElement>(null!);
+    const csvInputRef = React.useRef<HTMLInputElement>(null!);
+
+    const loadPlaylist = async () => {
+        const videoIds = await window.api.getListData(urlInputRef.current.value);
+        if (videoIds === undefined) throw Error('URLが間違っています！');
+        const result = await window.api.getVideoData(videoIds);
+        props.setPlaylistData(result);
+    };
+
+    const outputJpeg = async () => {
+        const songTable = document.getElementById('listdata-table');
+        if (songTable === null) return;
+        const imageUrl = await domtoimage.toJpeg(songTable);
+        const downloadElement = document.createElement('a');
+        downloadElement.href = imageUrl;
+        downloadElement.download = '100sen';
+        downloadElement.click();
+    };
+
+    const outputCsv = async () => {
+
+    };
+
     return (
         <div id="viewer-menu">
             <div id="from-wrapper">
                 <div className="from-item-wrapper">
-                    <label><input type="radio" name="from" id="fromUrl" defaultChecked />URLから読み込み</label>
-                    <input type="text" id="url-inputbox" ref={props.urlInputRef} />
+                    <label><input type="radio" id="fromUrl" defaultChecked />URLから読み込み</label>
+                    <input type="text" id="url-inputbox" ref={urlInputRef} />
                 </div>
                 <div className="from-item-wrapper">
-                    <label><input type="radio" name="from" id="fromCSV" />CSVから読み込み</label>
-                    <input type="file" id="csv-filebox" ref={props.csvInputRef} />
+                    <label><input type="radio" id="fromCSV" />CSVから読み込み</label>
+                    <input type="file" id="csv-filebox" ref={csvInputRef} />
                 </div>
                 <div className="from-load">
-                    <button id="load-btn" onClick={props.loadList}>表示</button>
+                    <button id="load-btn" onClick={loadPlaylist}>表示</button>
                 </div>
             </div>
             <div className="to-wrapper">
                 <div className="to-item-wrapper">
-                    <button id="output-jpeg-btn" onClick={props.outputJpeg}>CSVファイルとして出力</button>
+                    <button id="convert-btn" onClick={outputJpeg}>画像として出力</button>
                 </div>
                 <div className="to-item-wrapper">
-                    <button id="convert-btn" onClick={props.outputJpeg}>画像として出力</button>
+                    <button id="output-jpeg-btn" onClick={outputCsv}>CSVファイルとして出力</button>
                 </div>
             </div>
         </div>
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import domtoimage from 'dom-to-image';
 
 import PlaylistTable from './PlaylistTable';
 import { SongDataForTable } from './types';
@@ -8,35 +7,11 @@ import ViewerMenu from "./ViewerMenu";
 
 const RootDiv: React.FC = () => {
     const [playlistData, setPlaylistData] = React.useState<SongDataForTable[]>([]);
-    const urlInputBox = React.useRef<HTMLInputElement>(null!);
-    const csvInpurBox = React.useRef<HTMLInputElement>(null!);
-
-    const handleOnClick = async () => {
-        const videoIds = await window.api.getListData(urlInputBox.current.value);
-        if (videoIds === undefined) throw Error('URLが間違っています！');
-        const result = await window.api.getVideoData(videoIds);
-        setPlaylistData(result);
-    };
-
-    const convertPhotoClick = async () => {
-        const songTable = document.getElementById('listdata-table');
-        if (songTable === null) return;
-        const imageUrl = await domtoimage.toJpeg(songTable);
-        const downloadElement = document.createElement('a');
-        downloadElement.href = imageUrl;
-        downloadElement.download = '100sen';
-        downloadElement.click();
-    };
 
     return (
         <>
-            <ViewerMenu
-                urlInputRef={urlInputBox}
-                csvInputRef={csvInpurBox}
-                loadList={handleOnClick}
-                outputJpeg={convertPhotoClick}
-            />
-            <PlaylistTable tableData={playlistData}/>
+            <ViewerMenu setPlaylistData={setPlaylistData} />
+            <PlaylistTable tableData={playlistData} />
         </>
     );
 }
