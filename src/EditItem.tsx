6eecc76631e880c5b4cb61feef414a817a4c68c6
fanta-git <fantaji4957t@gmail.com>
2022-05-29 14:23:25 +0900

    add: ボタンとかの仮設置 #21

--- src/EditItem.tsx
+++ src/EditItem.tsx
@@ -27,20 +27,39 @@ const EditItem: React.FC<Props> = ({ tableData, selectedItem, setSelectedItem })
         <div id="edit-item" className="viewer-menu">
             <div id="update-item">
                 <div className="item-wrapper">
-                    {
-                        UPDATE_TARGET_ALL.map(([name, jp], i) => (
-                            <label key={i}>
-                                <input type="radio" name="update-target" value={name} onChange={() => setUpdateTarget(name)} checked={updateTarget === name} />
-                                {jp}
-                            </label>
-                        ))
-                    }
+                    <select id="update-target" onChange={e => setUpdateTarget(e.target.value as typeof updateTarget)} value={updateTarget}>
+                        {
+                            UPDATE_TARGET_ALL.map(([name, jp], i) => (
+                                <option key={i} value={name} >
+                                    {jp}
+                                </option>
+                            ))
+                        }
+                    </select>
+                    <button onClick={() => updateTableData(tableData.getData(selectedItem)?.original[updateTarget] ?? '')}>変更取消</button>
                 </div>
                 <div className="item-wrapper">
-                    <input type="text" id="update-input" value={updateInput} onChange={e => updateTableData(e.target.value)} />
+                    <textarea id="update-input" value={updateInput} onChange={e => updateTableData(e.target.value)} />
                 </div>
-                <button onClick={() => setSelectedItem(-1)}>選択解除</button>
-                <button onClick={() => updateTableData(tableData.getData(selectedItem)?.original[updateTarget] ?? '')}>読み込み時のデータに戻す</button>
+            </div>
+            <div id="change-display">
+                <div className="item-wrapper">
+                    <label>背景色<input type="color" id="background-color" /></label>
+                </div>
+                <div className="item-wrapper">
+                    <label>タイトル文字サイズ<input type="number" className="char-size" step={0.1} /></label>
+                </div>
+                <div className="item-wrapper">
+                    <label>投稿者名文字サイズ<input type="number" className="char-size" step={0.1} /></label>
+                </div>
+            </div>
+            <div id="select-item">
+                <div className="item-wrapper">
+                    <label><input type="checkbox" id="multiple-selection" />複数選択</label>
+                </div>
+                <div className="item-wrapper">
+                    <button onClick={() => setSelectedItem(-1)}>選択解除</button>
+                </div>
             </div>
         </div>
     );
